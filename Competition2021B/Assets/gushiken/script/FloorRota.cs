62686e9c113b0500ab57590b2e711c463b6b2f64
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2021-10-13 17:10:45 +0900

    傾く床です途中です

--- Competition2021B/Assets/gushiken/script/FloorRota.cs
+++ Competition2021B/Assets/gushiken/script/FloorRota.cs
@@ -7,26 +7,58 @@ public class FloorRota: MonoBehaviour
     Transform Floor;
 
     // ワールド座標を基準に、回転を取得
-    Vector3 FloorRota1 = new Vector3(0, 0, 0);
+    public Vector3 FloorRota1 = new Vector3(0, 0, 0);
+
+    private float oriYpos = 0;
+
+    public float speed = 0;
+
+    
 
     private void Start()
     {
         Floor = this.transform;
+
+        //開始時のｙ座標を格納
+        oriYpos = Floor.transform.rotation.y;
     }
 
 
     void Update()
     {
+   
+       // if (Floor.rotation.x * 180 <= 46.0f && Floor.rotation.x * 180 >= -46.0f && Floor.rotation.z * 180 < 46.0f && Floor.rotation.z * 180 > -46.0f)
+        //{
 
-        if (Floor.rotation.x*180<=30.0f && Floor.rotation.x*180>=-30) {
-            FloorRota1.x = -Input.GetAxis("Vertical");
-            FloorRota1.y = 0;
-           // FloorRota1.z = Input.GetAxis("Horizontal");
 
-            Floor.transform.Rotate(FloorRota1);
+            FloorRota1.x += speed * -Input.GetAxis("Vertical");
+            FloorRota1.y = oriYpos;
+            FloorRota1.z += speed * Input.GetAxis("Horizontal");
+
+
+        // }
+
+
+        //transform.rotation = new Quaternion(transform.rotation.x,0,transform.rotation.z,0);
+        //transform.SetPositionAndRotation(Floor.position, Quaternion.e);/*(Floor.rotation.x,0.0f,Floor.rotation.z,0.0f)*/
+
+
+        if (Mathf.Abs(Floor.rotation.x * 180  +  FloorRota1.x)> 46.0f)
+        {
+            FloorRota1.x = 0.0f;
+           
+        }
+        Debug.Log(Mathf.Abs(Floor.rotation.x * 180 + FloorRota1.x));
+
+        if (Mathf.Abs(Floor.rotation.z * 180 + FloorRota1.z) > 46.0f) {
+
+            FloorRota1.z = 0.0f;
+
         }
 
-       Debug.Log(Floor.localEulerAngles);
+        Floor.transform.Rotate(FloorRota1,Space.World);
+
+       // Debug.Log(Floor.rotation.x);
         // transform.Rotate(new Vector3(0, 0, 5));
     }
 }
---
+++ Competition2021B/Assets/gushiken/script/FloorRota2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorRota2 : MonoBehaviour
+{
+
+    public Vector3 FloorRota = new Vector3(0,0,0);
+
+
+    void Start()
+    {
+        
+    }
+
+
+   
+
+
+    void Update()
+    {
+        //ここらへん
+        FloorRota.x += -Input.GetAxis("Vertical");
+        FloorRota.z += Input.GetAxis("Horizontal");
+
+
+
+        //ここらへん
+
+      Quaternion rotate = Quaternion.Euler(FloorRota.x, 0, FloorRota.z);
+
+        this.transform.rotation = rotate;
+
+
+    }
+}
