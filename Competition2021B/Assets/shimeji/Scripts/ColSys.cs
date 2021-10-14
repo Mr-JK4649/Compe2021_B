8e7a5bd7171a1e0c2a6939f5288c37d21b84c872
Bologost <bio0730r@gmail.com>
2021-10-14 14:39:05 +0900

    自分のシーンをいじったくらいかな

---
+++ Competition2021B/Assets/shimeji/Scripts/ColSys.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+
+public interface ICollider
+{
+    /// <summary>
+    /// 球との当たり判定
+    /// </summary>
+    bool CheckSphere(ISphere collider);
+}
+
+public interface ISphere
+{
+    /// <summary>
+    /// 中心のローカル座標
+    /// </summary>
+    Vector3 Center { get; }
+    /// <summary>
+    /// 半径
+    /// </summary>
+    float Radius { get; }
+    /// <summary>
+    /// 中心のワールド座標
+    /// </summary>
+    Vector3 WorldCenter { get; }
+}
