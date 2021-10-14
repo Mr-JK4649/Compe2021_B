3e66ebb6d6242ac4d01beeccdbffebe76dbdb652
yoshiyuki-2 <yoshiyuki2124357@gmail.com>
2021-10-14 14:14:10 +0900

    ボールが一定距離移動したらカメラが追従を開始するスクリプトを書きました。

---
+++ Competition2021B/Assets/yama/Scripts/MoveTheVirtualBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTheVirtualBall : MonoBehaviour
+{
+    Transform obj;
+    Vector3 moveSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = this.transform;
+        moveSpeed = new Vector3(0.001f, -0.000f, 0.001f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.position += moveSpeed;
+        obj.SetPositionAndRotation(obj.position, obj.rotation);
+    }
+}
