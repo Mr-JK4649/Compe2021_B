b2465924833f1f61beb7568f04e613a51f19eed3
Bologost <bio0730r@gmail.com>
2021-10-21 13:29:17 +0900

    ボールの処理をなんか変えた

--- Competition2021B/Assets/shimeji/Scripts/BallGravity.cs
+++ Competition2021B/Assets/shimeji/Scripts/BallGravity.cs
@@ -7,6 +7,10 @@ public class BallGravity : MonoBehaviour
     [SerializeField] private float gravity = 0f;
     [SerializeField,Tooltip("落ちる速さ(毎秒毎秒)")] private float speed = 0f;
     [SerializeField] private bool isFall = true;
+    [SerializeField] private bool isWall = false;
+
+    //自身のRididBody
+    private Rigidbody rb;
 
     //床の法線ベクトルのやし
     public GetNormals gn;
@@ -19,12 +23,33 @@ public class BallGravity : MonoBehaviour
     //床の法線
     private Vector3 floorNor;
 
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
     private void FixedUpdate()
     {
 
-        moveVec.x = floorNor.x * spd;
-        moveVec.y = 0;
-        moveVec.z = floorNor.z * spd;
+        //ボールのRigidbodyがSleep状態なら起こす
+        if (rb.IsSleeping())
+        {
+            rb.WakeUp();
+        }
+        
+        //壁に当たってなければ床の斜面に沿って移動する
+        //当たった場合速度を減衰させる
+        if (!isWall)
+        {
+            moveVec.x += floorNor.x * spd * Time.deltaTime;
+            moveVec.z += floorNor.z * spd * Time.deltaTime;
+        }
+        if (isWall) {
+            moveVec.x /= 2.0f;
+            moveVec.y /= 2.0f;
+            moveVec.z /= 2.0f;
+        }
+
 
         if (isFall)
         {
@@ -41,17 +66,35 @@ public class BallGravity : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isFall = false;
+        if (collision.gameObject.tag == "Floor")
+        {
+            isFall = false;
+        }
+
+        if(collision.gameObject.tag == "Wall"){
+            isWall = true;
+            Vector3 wn = collision.contacts[0].normal;
+            moveVec = Vector3.Reflect(moveVec, wn);
+            Debug.Log(wn);
+        }
+
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        floorNor = gn.GetNormal();
+        if (collision.gameObject.tag == "Floor")
+        {
+            //床の法線を取得
+            floorNor = gn.GetNormal();
+        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        isFall = true;
+        if(collision.gameObject.tag == "Floor")
+            isFall = true;
+        if (collision.gameObject.tag == "Wall")
+            isWall = false;
     }
 
 }
