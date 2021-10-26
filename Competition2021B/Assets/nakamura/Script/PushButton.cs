21f60db49d292c4d1b3e04fe4b58f683139379ed
ko-nayuki <nakamurayuuki0629@icloud.com>
2021-10-26 10:16:28 +0900

    ポーズメニューのprefabを更新/仮のタイトルシーンの追加

---
+++ Competition2021B/Assets/nakamura/Script/PushButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PushButton : MonoBehaviour
+{
+
+    void Update()
+    {
+        
+    }
+
+    public void NextMain()
+    {
+        Debug.Log("めいん");
+        SceneManager.LoadScene("MainScene");
+    }
+}
