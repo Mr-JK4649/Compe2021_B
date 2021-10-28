ce4891ae7281351d26f15977bce2b7021d85b6dd
Bologost <bio0730r@gmail.com>
2021-10-28 13:41:58 +0900

    音が、、、止まったー－－ッ！！！

--- Competition2021B/Assets/shimeji/Scripts/AudioProcess.cs
+++ Competition2021B/Assets/shimeji/Scripts/AudioProcess.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class AudioProcess : MonoBehaviour
 {
-    public AudioClip[] seClip;
     private AudioSource audioSource;
+    public AudioSource bgmAudioSource;
+    public AudioClip[] seClip;
+
+    //ポーズ処理
+    pause pauseCls;
 
     void Start()
     {
         audioSource = this.GetComponent<AudioSource>();
+        pauseCls = GameObject.Find("Pause").GetComponent<pause>();
+    }
+
+    private void Update()
+    {
+        if (pauseCls.IsPause()) bgmAudioSource.Stop();
+        if (!pauseCls.IsPause()) bgmAudioSource.Play();
+        
     }
 
     public void ShotSE(int num){
