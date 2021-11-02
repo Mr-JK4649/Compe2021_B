01512ac1d134c9633d329c942106363b895130ac
Bologost <bio0730r@gmail.com>
2021-11-02 10:04:18 +0900

    ｔるｇ

---
+++ Competition2021B/Assets/shimeji/Scripts/Trigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private TriggerEvent te = new TriggerEvent();
+    [SerializeField] private TriggerEvent onConEn = new TriggerEvent();
+    [SerializeField] private TriggerEvent onConEx = new TriggerEvent();
+    [SerializeField] private TriggerEvent onConSt = new TriggerEvent();
+
+    public void FuncA(){ te.Invoke();}
+    public void OnCollisionEnter (Collision other){ onConEn.Invoke();}
+    public void OnCollisionExit (Collision other){ onConEx.Invoke();}
+    public void OnCollisionStay (Collision other){ onConSt.Invoke();}
+
+    [Serializable]
+    public class TriggerEvent : UnityEvent
+    {
+    }
+
+}
+
+
+//UnityEventの引数の書き方
+//public class TE_Class_Name : UnityEvent<argument>{}
+// 以下、例
+
+//public class TE_Class_Name : UnityEvent<Collider>{}
+//public class TE_Class_Name : UnityEvent<Vector3>{}
