05b3b82500f1b04437a9046883577af0407a3961
yoshiyuki-2 <yoshiyuki2124357@gmail.com>
2021-11-11 16:50:15 +0900

    デッドゾーンの追加に成功しました。

--- Competition2021B/Assets/yama/Scripts/TrackingCamera.cs
+++ Competition2021B/Assets/yama/Scripts/TrackingCamera.cs
@@ -1,11 +1,16 @@
-﻿#define DEBUG_YAMA
+﻿#define DEBUG_YAMA_ON_2
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TrackingCamera : MonoBehaviour
 {
+// 定数の宣言、定義---
+    const float BALL_MOVE_FREE_RANGE = 0.15f;  // ボールが自由に動ける範囲
+
+// 変数の宣言---
     Transform trackCamera;         // カメラのトランスフォーム
     public Transform ball;         // ボールのトランスフォーム
     Vector3 disToBall;             // ボールまでの距離
@@ -14,14 +19,14 @@ public class TrackingCamera : MonoBehaviour
     float interpolationRate;       // ボールとカメラとの距離の補間割合
     bool cameraMoveFlg;            // カメラが移動するかを制御　true:移動する    false:移動しない
     bool cameraPosFitFlg;          // カメラがボールに張り付いたらtrue
-
-
-
+    
+// 関数ここから---
     // Start is called before the first frame update
     void Start()
     {
         trackCamera = this.transform;
         disToBall = new Vector3(0.0f, 1.4f, 0.4f);
+        //disToBall = Vector3.zero;
         f_SetCameraPosAndRotation();
         ballStartPos = ball.position;
         //BallStartPosDiff = Vector3.zero;
@@ -60,6 +65,12 @@ public class TrackingCamera : MonoBehaviour
     {
         const float ADD_INTER_VALU = 0.03f;    // interpolationValuに加算する値
 
+        Vector2 mv = f_Move();
+
+        trackCamera.position += new Vector3(mv.x, 0, mv.y);
+        ballStartPos += new Vector3(mv.x, 0, mv.y);
+
+        /*
         if (cameraPosFitFlg == true)
         {
             trackCamera.position = new Vector3(ball.position.x + disToBall.x,
@@ -68,6 +79,9 @@ public class TrackingCamera : MonoBehaviour
         }
         else
         {
+            float dis;              // ボールの初期地点から現在地点までの距離
+            dis = Vector3.Distance(ball.position, ballStartPos);
+
             // カメラを徐々にボールに近づける
             trackCamera.position = Vector3.Lerp(trackCamera.position, 
                                                 new Vector3(ball.position.x + disToBall.x,
@@ -88,6 +102,7 @@ public class TrackingCamera : MonoBehaviour
                 cameraPosFitFlg = true;
             }
         }
+        */
     }
 
     /// <summary>
@@ -97,8 +112,23 @@ public class TrackingCamera : MonoBehaviour
     void f_BallMoveCheck()
     {
         float dis;              // ボールの初期地点から現在地点までの距離
-        dis = Vector3.Distance(ball.position, ballStartPos);
-        
+        //dis = Vector3.Distance(ball.position, ballStartPos);
+        dis = Vector2.Distance(new Vector2(ball.position.x, ball.position.z), 
+                               new Vector2(ballStartPos.x, ballStartPos.z));
+
+
+        if(dis < BALL_MOVE_FREE_RANGE)
+        {
+            cameraMoveFlg = false;
+            cameraPosFitFlg = false;
+            interpolationRate = 0.0f;
+        }
+        else
+        {
+            cameraMoveFlg = true;
+        }
+
+        /*
         // カメラが動いている場合
         if (cameraMoveFlg == true)
         {
@@ -123,7 +153,9 @@ public class TrackingCamera : MonoBehaviour
                 cameraMoveFlg = true;
             }
         }
-#if DEBUG_YAMA
+        */
+
+#if DEBUG_YAMA_ON
         Debug.Log("cameraMoveFlg");
         Debug.Log(cameraMoveFlg);
         Debug.Log("cameraPosFitFlg");
@@ -138,5 +170,24 @@ public class TrackingCamera : MonoBehaviour
     {
         trackCamera.position = ball.position;
         trackCamera.SetPositionAndRotation(ball.position + disToBall, f_ReturnAngle_CameraToBall());
+    }
+
+    Vector2 f_Move()
+    {
+        float dis;
+        dis = Vector2.Distance(new Vector2(ball.position.x, ball.position.z),
+                               new Vector2(ballStartPos.x, ballStartPos.z));
+        float disHowMuch;
+        disHowMuch = BALL_MOVE_FREE_RANGE / dis;  // 球初期地点から球までの距離の倍率 = 移動開始距離 / 球初期地点から球までの距離
+        disHowMuch = 1 - disHowMuch;              //  球から円周までの距離を求めるのに必要な倍率 = 全体 - 球初期地点から球までの距離の倍率
+
+#if DEBUG_YAMA_ON_2
+        Debug.Log(disHowMuch);
+#endif
+
+        Vector2 moveVule = new Vector2((ball.position.x - ballStartPos.x) * disHowMuch,
+                                       (ball.position.z - ballStartPos.z) * disHowMuch);
+        
+        return moveVule;
     }
 }
