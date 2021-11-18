b32c6d3f65916d81c4100fa6a793660e48ab3123
GushikenKaito <kaito-gushiken19h@stu.kbc.ac.jp>
2021-11-18 15:18:35 +0900

    スピードが保持されるバグを修正、ブレーキをかける処理のバグが新しく発生しましたが 大きいバグではないのでそのままプッシュします

--- Competition2021B/Assets/gushiken/script/FloorRota2.cs
+++ Competition2021B/Assets/gushiken/script/FloorRota2.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
 
 public class FloorRota2 : MonoBehaviour
 {
@@ -8,12 +11,16 @@ public class FloorRota2 : MonoBehaviour
     public Vector3 FloorRota = new Vector3(0,0,0);
 
     public float speed = 0;
+    //private float speedBox = 0;
 
     private float testFlame = 0;   //テスト用
     private float Box = 0.0f;      //床の角度を0度にするための処理に使う変数
+    private float Box2 = 0.0f;
 
     private float tiltx=0; //x軸の傾けている方向を入れる
     private float tiltz=0; //y軸の傾けている方向を入れる
+    private float tilta = 0;
+    private float Oldtilta = 0;
 
     private float OldInputx = 0; //前回入力したx軸入力を入れる
     private float OldInputz = 0; //前回入力したｚ軸の入力を入れる
@@ -40,18 +47,29 @@ public class FloorRota2 : MonoBehaviour
     {
         //ここらへん
         //TiltSpeed();
-        if(stopp==0) {
-            FloorRota.x += speed * -Input.GetAxis("Vertical");
-            FloorRota.z += speed * Input.GetAxis("Horizontal");
-        }else if (stopp==1)
-        {
-            FloorRota.x += speed * Input.GetAxis("Vertical");
-            FloorRota.z += speed * -Input.GetAxis("Horizontal");
 
-        }
+        //GETAXISを使ってスピードを上げる
+        //GETAXISは負の位置：-1 ニュートラル：0 正の位置：1になる
+        //どの方向でも傾いている間にスピードだけが加算されていき逆に入力すると減算した後に入力した方向に移動する
 
-        TiltSpeed();
+        //testFlame=Mathf.Abs(speed*Input.GetAxis("Vertical"));
 
+        //if (FloorRota.x < 30 && FloorRota.x > -30 && FloorRota.z < 30 && FloorRota.z > -30) {
+        //    if (stopp == 0)
+        //    {
+                FloorRota.x += speed * -Input.GetAxisRaw("Vertical");
+                FloorRota.z += speed * Input.GetAxisRaw("Horizontal");
+        //    }
+        //    else {
+        //        FloorRota.x += speed * +Input.GetAxisRaw("Vertical");
+        //        FloorRota.z += speed * -Input.GetAxisRaw("Horizontal");
+
+        //    }
+        //}
+
+
+
+    TiltSpeed();
 
         if (FloorRota.x > 30)       //**xの傾きをを制限かける処理
         {
@@ -79,7 +97,7 @@ public class FloorRota2 : MonoBehaviour
         //ここらへん
 
 
-        Debug.Log(speed);
+        //Debug.Log(testFlame);
         //Debug.Log(tiltx);
        // Debug.Log(tiltz);
 
@@ -98,16 +116,27 @@ public class FloorRota2 : MonoBehaviour
         float OneFlamemove = 30.0f/8100.0f;  //水平から30度までの
         
         
+        //tilt
+        tiltx = Input.GetAxisRaw("Vertical");
+        tilta = tiltx;
+        tiltz = 3 + Input.GetAxisRaw("Horizontal");
 
-        tiltx = Input.GetAxis("Vertical");
-        tiltz = Input.GetAxis("Horizontal");
+        if (tiltx == 0)
+        {
+            tilta = tiltz;
+        }
 
-        if (tiltx==1 && tiltz ==1 || tiltx==-1 && tiltz==-1 || tiltx==-1 && tiltz==1 || tiltx==1 && tiltz==-1)//斜めの操作を制限
+        
+        
+
+
+
+        if (tiltx==1 && tiltz ==4 || tiltx==-1 && tiltz==2 || tiltx==-1 && tiltz==4 || tiltx==1 && tiltz==2)//斜めの操作を制限
         {
             
           speed = 0.0f;
 
-        }else if(tiltx==0&&tiltz==0){//ニュートラルの時
+        }else if(tiltx==0&&tiltz==3){//ニュートラルの時
 
             if (speed>0)
             {
@@ -170,60 +199,37 @@ public class FloorRota2 : MonoBehaviour
                 }
                 /////////--------------------------ここまで
 
-                Debug.Log(Box);
+               // Debug.Log(Box);
             }//////////////////////////////////////////////////////////傾いていたら
 
         }
         else{//十字キーを操作している時
             testFlame++;
 
-            if (speed >= 0 && tiltx == 1 && OldInputx == -1 || speed >= 0 && tiltx == -1 && OldInputx == 1 ||
-                speed >= 0 && tiltz == 1 && OldInputz == -1 || speed >= 0 && tiltz == -1 && OldInputz == 1)
+
+            if (speed>0&&tilta!=Oldtilta)
             {
+
+                speed -= OneFlamemove*1.6f;
                 stopp = 1;
-                speed -= OneFlamemove; //切り替えした時にこの処理でスピードを0にして下の処理に移動
-               
-                
             }
             else
             {
+                speed += OneFlamemove;
+                Oldtilta = tilta;
                 stopp = 0;
-                speed += OneFlamemove; //通常の操作時の移動
-                OldInputx = tiltx;
-                OldInputz=tiltz;
-
             }
 
-
-
-
-            if (FloorRota.x>=30)
+            if (testFlame==90)
             {
-                Debug.Log(testFlame);
+                //フレームを測る用、ブレークポイントを着けて使用
+                //xが30度傾いたら止める
+                Debug.Log(FloorRota.x);
             }
-
-
 
            // Debug.Log(testFlame);
         }
 
-    }
-
-    void aaa()
-    {
-
-        if (tiltx != OldInputx || tiltz != OldInputz)
-        {
-            speed -= 30.0f / 8100.0f;
-        }
-        else
-        {
-            FloorRota.x += speed * -Input.GetAxis("Vertical");
-            FloorRota.z += speed * Input.GetAxis("Horizontal");
-        }
-
-     
-    
     }
 
 
