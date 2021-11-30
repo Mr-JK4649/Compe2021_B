c4c5196b71a00c06e8586285bde6b9ed033ed112
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-11-30 23:10:04 +0900

    新しいスコアと当たり判定

---
+++ Competition2021B/Assets/Hachimine/Script/HitCheck/HitCheckScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCheckScript : MonoBehaviour
+{
+    public bool HitCheck(GameObject p, GameObject item)
+    {
+        Vector3 item_pos = item.transform.position;//座標保存
+
+        Vector3 p_pos = p.transform.position;//プレイヤー座標
+        Debug.Log(item);
+        Vector3 Def = p_pos - item_pos;
+
+        float d = Def.magnitude;
+
+        float p_radius = p.transform.localScale.x / 2;
+        float item_radius = item.transform.localScale.x / 2;
+
+        bool hit_flg = false;
+        if (d <= p_radius + item_radius)
+        {
+            hit_flg = true;
+        }
+        else
+        {
+            hit_flg = false;
+        }
+
+        return hit_flg;
+    }
+
+}
---
+++ Competition2021B/Assets/Hachimine/Script/HitCheck/ScorePlus.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorePlus : MonoBehaviour
+{
+
+    GameObject CoinManeger;
+    CoinGetText coingettext;
+    public GameObject getCoinFX;
+
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    GameObject hit_objplu;
+
+    HitCheckScript hitcheck_scoreplus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.Find("Ball"); //プレイヤーメイ
+        }
+        if (hitcheck_scoreplus == null)
+        {
+            hit_objplu = GameObject.Find("CoinManager"); //HitCheckScriptを使いたい
+        }
+        hitcheck_scoreplus = hit_objplu.GetComponent<HitCheckScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoinManeger = GameObject.Find("CoinManager");
+        coingettext = CoinManeger.GetComponent<CoinGetText>();
+        CoinPlus();
+    }
+    void CoinPlus()
+    {
+        if (hitcheck_scoreplus.HitCheck(player, this.gameObject))
+        {
+            coingettext.CoinCollect_Point += 1; //コインを取った時の数値に＋１する。
+            GameObject effe = Instantiate(getCoinFX, this.transform.position, Quaternion.identity);
+            Destroy(effe, 1.0f);
+            Destroy(this.gameObject);
+        }
+    }
+}
