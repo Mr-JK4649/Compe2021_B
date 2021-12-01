3a3242dcb1c36875cea820f403f02dd15fa5b4be
HachimineDaiki <daiki-hachimine-19h@kbc.ac.jp>
2021-12-01 15:37:49 +0900

    ステージ全部にスクリプトを用意した。

---
+++ Competition2021B/Assets/Hachimine/Script/HitCheck/PlayerHitCheck.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitCheck : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    [SerializeField]
+    GameObject hit_obj;
+
+
+    HitCheckScript hitchek;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.Find("Ball"); //プレイヤー名
+        }
+
+        if (hitchek == null)
+        {
+            hit_obj = GameObject.Find("CoinManager"); //オブジェクト名前
+        }
+
+        hitchek = hit_obj.GetComponent<HitCheckScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hitchek.HitCheck(player, this.gameObject))
+        {
+            Debug.Log("当たっている");
+            //Destroy(this.gameObject);
+
+        }
+        else
+        {
+            Debug.Log("当たってない");
+        }
+
+    }
+}
