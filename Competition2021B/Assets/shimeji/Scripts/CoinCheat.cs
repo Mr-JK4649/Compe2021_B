fb9a4aed2698dc410e648e9aeffa1bb2db252383
Bologost <bio0730r@gmail.com>
2021-12-02 10:38:36 +0900

    コインチート

---
+++ Competition2021B/Assets/shimeji/Scripts/CoinCheat.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCheat : MonoBehaviour
+{
+    
+    void Start()
+    {
+        GameObject coins = GameObject.Find("CoinGroup 1");
+        int num = coins.transform.childCount;
+        for (int i = 0; i < num; i++) {
+            coins.transform.GetChild(i).gameObject.AddComponent<Magnet>();
+        }
+    }
+}
---
+++ Competition2021B/Assets/shimeji/Scripts/Magnet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    bool isMagnet = true;
+    Transform tra;
+    Vector3 pos;
+
+    private void Start()
+    {
+        tra = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+        if (isMagnet) {
+            pos = tra.position;
+
+            this.transform.position = Vector3.Lerp(this.transform.position, pos, 0.05f);
+        }
+    }
+}
