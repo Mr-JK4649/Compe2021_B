85605bba4c33fe2a62dbe48acc1ce39e4f0dd078
Bologost <bio0730r@gmail.com>
2021-12-02 13:15:40 +0900

    特に意味はない

---
+++ Competition2021B/Assets/shimeji/Scripts/LookAtBall.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtBall : MonoBehaviour
+{
+    Transform ball;
+
+    private void Start()
+    {
+        ball = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.LookAt(ball);
+    }
+}
---
+++ Competition2021B/Assets/shimeji/Scripts/OnOutLine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOutLine : MonoBehaviour
+{
+
+    private void Start()
+    {
+        //transform.parent.gameObject.GetComponent<Outline>().enabled = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player") {
+
+            //transform.parent.gameObject.GetComponent<Outline>().enabled = true;
+            
+        }
+    }
+}
